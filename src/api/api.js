4646867f2b29b245e74c5abab5ceae3d2a6d508c
zcxiaobao <444186976@qq.com>
2020-02-18 12:11:31 +0800

    修复省级地区数据渲染的问题

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,41 @@
 import axios from '@/axios.js'
-
+const provinces = {
+  // 23个省
+  台湾: 'taiwan',
+  河北: 'hebei',
+  山西: 'shanxi',
+  辽宁: 'liaoning',
+  吉林: 'jilin',
+  黑龙江: 'heilongjiang',
+  江苏: 'jiangsu',
+  浙江: 'zhejiang',
+  安徽: 'anhui',
+  福建: 'fujian',
+  江西: 'jiangxi',
+  山东: 'shandong',
+  河南: 'henan',
+  湖北: 'hubei',
+  湖南: 'hunan',
+  广东: 'guangdong',
+  海南: 'hainan',
+  四川: 'sichuan',
+  贵州: 'guizhou',
+  云南: 'yunnan',
+  陕西: 'shanxi1',
+  甘肃: 'gansu',
+  青海: 'qinghai', // 5个自治区
+  新疆: 'xinjiang',
+  广西: 'guangxi',
+  内蒙古: 'neimenggu',
+  宁夏: 'ningxia',
+  西藏: 'xizang', // 4个直辖市
+  北京: 'beijing',
+  天津: 'tianjin',
+  上海: 'shanghai',
+  重庆: 'chongqing', // 2个特别行政区
+  香港: 'xianggang',
+  澳门: 'aomen'
+}
 const api = {
   getNcovData() {
     const ncovData = localStorage.getItem('ncovData')
@@ -22,6 +58,14 @@ const api = {
         key: 'e82f86af9a0391032dc487dcce8f6b69'
       }
     })
+  },
+  getProvinceData(province) {
+    if (province !== 'china') {
+      return axios.get(`/json/province/${provinces[province]}.json`)
+    } else {
+      // F:\duyiSpace\vue\echarts-vue\src\assets\common\map\json\china.json
+      return axios.get('/json/china.json')
+    }
   }
 }
 export default api
--- src/views/ncov-map.vue
+++ src/views/ncov-map.vue
@@ -3,8 +3,8 @@
 </template>
 <script>
 import echarts from 'echarts'
-import 'echarts/map/js/china.js'
-import '@/assets/js/data.js'
+// import 'echarts/map/js/china.js'
+// import '@/assets/js/data.js'
 import api from '@/api/api.js'
 export default {
   data() {
@@ -60,7 +60,6 @@ export default {
       if (!localStorage.getItem('ncovDataAll')) {
         localStorage.setItem('ncovDataAll', JSON.stringify(res))
       }
-      console.log(res)
     })
   },
   mounted() {
@@ -72,10 +71,6 @@ export default {
   methods: {
     _initEcharts() {
       this.myChart = echarts.init(this.$refs.ncov)
-      this.myChart.on('click', params => {
-        const data = this._searchCityNcovData(params.name)
-        this._renderMap(params.name, this._transformNcovData(data, false))
-      })
     },
     _searchCityNcovData(provinceName) {
       const index = this.ncovData.findIndex(
@@ -84,58 +79,72 @@ export default {
       return this.ncovData[index].cities
     },
     _renderMap(mapName, data) {
-      const option = {
-        title: {
-          show: true,
-          text: '疫情累计确诊地图',
-          top: '10%',
-          left: 'center'
-        },
-        tooltip: {
-          formatter(params) {
-            return `地区: ${params.name}
+      // axios.get
+      api.getProvinceData(mapName).then(res => {
+        echarts.registerMap(mapName, res)
+        const option = {
+          title: {
+            show: true,
+            text: '疫情累计确诊地图',
+            top: '10%',
+            left: 'center'
+          },
+          tooltip: {
+            trigger: 'item',
+            formatter(params) {
+              return `地区: ${params.name}
                     <br/>累计确诊: ${params.value}`
-          }
-        },
-        series: [
-          {
-            type: 'map',
-            map: mapName,
-            name: '疫情信息',
-            label: {
-              show: true
-            },
-            data: data
-          }
-        ],
-        visualMap: {
-          // 左下角的颜色区域
-          type: 'piecewise', // 定义为分段型 visualMap
-          min: 0,
-          max: 100000,
-          itemWidth: 40,
-          bottom: 60,
-          left: 20,
-          pieces: [
-            { gt: 10000, label: '>10000', color: '#4F070D' },
+            }
+          },
+          series: [
             {
-              gt: 1000,
-              lte: 10000,
-              label: '1000-10000',
-              color: '#811C24'
-            },
-            { gt: 500, lte: 999, label: '500-999', color: '#CB2A2F' },
-            { gt: 100, lte: 499, label: '100-499', color: '#CB5A4E' },
-            { gt: 10, lte: 99, label: '10-99', color: '#F59E83' },
-            { gt: 0, lte: 9, label: '1-9', color: '#FDEBCF' }, // (]
-            { value: 0, label: '0', color: '#FFF' } // [0]
-          ]
+              type: 'map',
+              map: mapName,
+              name: '疫情信息',
+              label: {
+                show: true
+              },
+              data: data
+            }
+          ],
+          visualMap: {
+            // 左下角的颜色区域
+            type: 'piecewise', // 定义为分段型 visualMap
+            min: 0,
+            max: 100000,
+            itemWidth: 40,
+            bottom: 60,
+            left: 20,
+            pieces: [
+              { gt: 9999, label: '>10000', color: '#4F070D' },
+              {
+                gt: 999,
+                lte: 10000,
+                label: '1000-10000',
+                color: '#811C24'
+              },
+              { gt: 499, lte: 999, label: '500-999', color: '#CB2A2F' },
+              { gt: 99, lte: 499, label: '100-499', color: '#CB5A4E' },
+              { gt: 9, lte: 99, label: '10-99', color: '#F59E83' },
+              { gt: 0, lte: 9, label: '1-9', color: '#FDEBCF' }, // (]
+              { value: 0, label: '0', color: '#FFF' } // [0]
+            ]
+          }
         }
-      }
-      this.myChart.setOption(option)
+        if (mapName === 'china') {
+          this.myChart.on('click', this.clickHandler)
+        } else {
+          this.myChart.off('click', this.clickHandler)
+        }
+        this.myChart.setOption(option)
+      })
+    },
+    clickHandler(params) {
+      const data = this._searchCityNcovData(params.name)
+
+      this._renderMap(params.name, this._transformNcovData(data, false))
     },
     _transformNcovData(list, isProvince = true) {
-      console.log(list)
       return list.map(item => {
         return {
           name: isProvince ? item.provinceShortName : item.cityName,
